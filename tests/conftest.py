c24bef9774f86dbd402d1b907da448a35d47ac64
Aidar_Mahmutov <armax045@gmail.com>
2025-04-17 22:44:14 +0400

    docker_ci, v7

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,11 @@ def browser(request):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
     driver = webdriver.Remote(command_executor=hub_url, options=options)
-    driver.set_window_size(1920, 1080)
+    # driver.set_window_size(1920, 1080)
     yield driver
     driver.quit()
 
